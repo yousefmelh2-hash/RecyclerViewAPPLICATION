535bc5845e8dbdbf7defef05712d73f7397a3ede
yousef <yousefmelh2@gmail.com>
2026-09-08 15:56:17 +0300

    version-3 pokemon update

--- app/src/main/java/com/example/yousef/ItemAdapter.java
+++ app/src/main/java/com/example/yousef/ItemAdapter.java
@@ -1,19 +1,76 @@
 package com.example.yousef;
 
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-/*
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.MyViewHolder> {
-    private List<String> itemList;
 
+    private List<Item> itemList;
+
+    // Constructor
+    public ItemAdapter(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    // Holds the Views inside one item_row
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        public MyViewHolder(View itemView) {
-        super(itemView);
 
+        TextView tvName;
+        TextView tvLastName;
+        ImageView ivPhoto;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvName);
+            tvLastName = itemView.findViewById(R.id.tvLastName);
+            ivPhoto = itemView.findViewById(R.id.ivPhoto);
         }
     }
-}*/
+
+    // Creates a new item_row
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent,
+            int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_row, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    // Puts the Item's data into the row
+    @Override
+    public void onBindViewHolder(
+            @NonNull MyViewHolder holder,
+            int position) {
+
+        Item item = itemList.get(position);
+
+        holder.tvName.setText(item.getName());
+        holder.tvLastName.setText(item.getLastName());
+
+        if (item.getPhotoID() != null) {
+            holder.ivPhoto.setImageURI(
+                    Uri.parse(item.getPhotoID())
+            );
+        }
+    }
+
+    // Number of items
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+}
--- app/src/main/java/com/example/yousef/ItemListActivity.java
+++ app/src/main/java/com/example/yousef/ItemListActivity.java
@@ -3,19 +3,27 @@ package com.example.yousef;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class ItemListActivity extends AppCompatActivity {
 ArrayList<Item> arrayList=new ArrayList<>();
+RecyclerView recyclerView;
+ItemAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_input_activity);
         initComponents();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter=new ItemAdapter(arrayList);
+        recyclerView.setAdapter(adapter);
     }
 
     private void initComponents() {
+        recyclerView=findViewById(R.id.recyclerView);
         arrayList=(ArrayList<Item>) getIntent().getSerializableExtra("array");
     }
 }
